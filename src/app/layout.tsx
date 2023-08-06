a9097785ef598becabe9fd89690187430f33a9b1
emad-salah <emads14@gmail.com>
2023-08-06 07:20:48 +0100

    Initialized project

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Auth App Assignment",
+  description: "Assignment to have a fully functional auth app using nextauth"
+};
+
+export default function RootLayout({
+  children
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
