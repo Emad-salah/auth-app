a223ddc58f1e73bc634b92d772308e2c97bafccd
emad-salah <emads14@gmail.com>
2023-08-07 10:51:10 +0100

    added discord auth to the providers

--- src/app/utils/auth.ts
+++ src/app/utils/auth.ts
@@ -1,11 +1,16 @@
 import { type NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import DiscordProvider from "next-auth/providers/discord";
 
 export const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+    }),
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID!,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET!
     })
   ],
   pages: {
